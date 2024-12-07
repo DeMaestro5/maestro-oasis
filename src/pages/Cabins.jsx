df9d3761865dbf82b194d1edef72b9e346780bfc
stephen <osstephen70@gmail.com>
2024-12-07 23:49:31 +0100

    feat/ added supabase for the backend data

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,22 @@
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+import { useEffect } from 'react';
+import Heading from '../ui/Heading';
+import Row from '../ui/Row';
+import { getCabins } from '../services/apiCabins';
 
 function Cabins() {
+  useEffect(function () {
+    getCabins().then((data) => {
+      console.log(data);
+    });
+  }, []);
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
+    <Row type='horizontal'>
+      <Heading as='h1'>All cabins</Heading>
       <p>TEST</p>
+      <img
+        src='https://ovupsjohmrucsrtbocgu.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-12-07T16%3A57%3A38.551Z'
+        alt='cabin'
+      />
     </Row>
   );
 }
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://ovupsjohmrucsrtbocgu.supabase.co';
+const supabaseKey =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92dXBzam9obXJ1Y3NydGJvY2d1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM1ODQxODAsImV4cCI6MjA0OTE2MDE4MH0.VkoNfCU1oOP5LjpJrL1ufY7reQTLIAMYlLebTN3kdQk';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
+// Compare this snippet from starter/services%20copy/apiSettings.j
