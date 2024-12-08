ba7d9fe0c2ecd11602d04aebda07dce24fa9166f
stephen <osstephen70@gmail.com>
2024-12-08 18:41:43 +0100

    added cabinTable

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -10,9 +12,19 @@ import PageNotFound from './pages/PageNotFound';
 import GlobalStyles from '../src/styles/GlobalStyle';
 import AppLayout from './ui/AppLayout';
 
+const query = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={query}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -29,7 +41,7 @@ function App() {
           <Route path='*' element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,23 +1,18 @@
-import { useEffect } from 'react';
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
-import { getCabins } from '../services/apiCabins';
+import CabinTable from '../features/cabins/CabinTable';
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => {
-      console.log(data);
-    });
-  }, []);
   return (
-    <Row type='horizontal'>
-      <Heading as='h1'>All cabins</Heading>
-      <p>TEST</p>
-      <img
-        src='https://ovupsjohmrucsrtbocgu.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2024-12-07T16%3A57%3A38.551Z'
-        alt='cabin'
-      />
-    </Row>
+    <>
+      <Row type='horizontal'>
+        <Heading as='h1'>All Cabin</Heading>
+        <p>filter & sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
