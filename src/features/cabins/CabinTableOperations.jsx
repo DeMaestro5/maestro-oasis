e26dd92b28b653ecf28a71c4391cb601fa5117ad
stephen <osstephen70@gmail.com>
2024-12-22 19:46:32 +0100

    feat/implemented the sorting feature for the cabins

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,4 +1,5 @@
 import Filter from '../../ui/Filter';
+import SortBy from '../../ui/SortBy';
 import TableOperations from '../../ui/TableOperations';
 
 function CabinTableOperations() {
@@ -10,6 +11,16 @@ function CabinTableOperations() {
           { label: 'All', value: 'all' },
           { label: 'With Discount', value: 'with-discount' },
           { label: 'No Discount', value: 'no-discount' },
+        ]}
+      />
+      <SortBy
+        options={[
+          { label: 'Sort by name (A-Z)', value: 'name-asc' },
+          { label: 'Sort by name (Z-A)', value: 'name-desc' },
+          { label: 'Sort by Price (low first)', value: 'regularPrice-asc' },
+          { label: 'Sort by Price (high first)', value: 'regularPrice-desc' },
+          { label: 'Sort by Capacity (low first)', value: 'maxCapacity-asc' },
+          { label: 'Sort by Capacity (high first)', value: 'maxCapacity-desc' },
         ]}
       />
     </TableOperations>
