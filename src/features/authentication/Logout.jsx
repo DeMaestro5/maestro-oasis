bc96f957247bf2f318c95013321d573b3fbb5973
stephen <osstephen70@gmail.com>
2024-12-27 10:25:55 +0100

    feat/ added the logout feature

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,18 @@
+import ButtonIcon from '../../ui/ButtonIcon';
+import { HiArrowRightOnRectangle } from 'react-icons/hi2';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
+
+function Logout() {
+  const { logout, isLoading } = useLogout();
+  function handleLogout() {
+    logout();
+  }
+  return (
+    <ButtonIcon onClick={handleLogout} disabled={isLoading}>
+      {!isLoading ? <HiArrowRightOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
+
+export default Logout;
